b5a3b5ba3dbb78a6d6d20f8f19a6d9e34635192a
adarsh-intel <adarsh.anand@intel.com>
2023-07-24 11:23:50 +0530

    Added Custom Button

---
+++ frontend/src/intel_components/Button.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../../../frontend/src/dist/css/dlux.css'; // dlux-intel css file
+import PropTypes from 'prop-types';
+
+const Button = ({ text,type, btn_type, onClick, onKeyPress }) => {
+  return (
+    <>
+      {/* btn_type can be primary, secondary, success, danger, warning, info, light, dark */}
+      <button
+        type={type}
+        className={`btn btn-${btn_type} me-1`}
+        onClick={onClick}
+        onKeyPress={onKeyPress}
+      >
+        {text}
+      </button>
+    </>
+  );
+};
+
+// PropTypes definition
+Button.propTypes = {
+  btn_type: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ]).isRequired,
+  onClick: PropTypes.func.isRequired,
+  onKeyPress: PropTypes.func.isRequired,
+  // default type prop is button
+};
+
+Button.defaultPorops = {
+  btn_type: 'primary',
+  type: 'button',
+}
+
+export default Button;
