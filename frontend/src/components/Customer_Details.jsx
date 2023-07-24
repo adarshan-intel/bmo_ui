fb8c69e058a2444923c632a9895938910f04442f
adarsh-intel <adarsh.anand@intel.com>
2023-07-24 12:17:57 +0530

    Changed forms of customer_details

--- frontend/src/components/Customer_Details.jsx
+++ frontend/src/components/Customer_Details.jsx
@@ -1,10 +1,11 @@
-import AddIcon from "@mui/icons-material/Add";
-import { TextField } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { customer_details } from "../data/Customer_details";
-import Navbar_Admin from "./Admin/Navbar_Admin";
+import AddIcon from '@mui/icons-material/Add';
+import { TextField } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { customer_details } from '../data/Customer_details';
+import Navbar_Admin from './Admin/Navbar_Admin';
+import Button from '../intel_components/Button';
 
 const Customer_Details = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ const Customer_Details = () => {
   const [customers, setcustomers] = useState(customer_details);
 
   // State variables for filtering and pagination
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
@@ -28,12 +29,12 @@ const Customer_Details = () => {
 
   // Define columns for the DataGrid
   const columns = [
-    { field: "Name", headerName: "Name", flex: 1 },
-    { field: "CustomerID", headerName: "CustomerID", flex: 1 },
-    { field: "Contact_Address", headerName: "Contact_Address", flex: 1 },
+    { field: 'Name', headerName: 'Name', flex: 1 },
+    { field: 'CustomerID', headerName: 'CustomerID', flex: 1 },
+    { field: 'Contact_Address', headerName: 'Contact_Address', flex: 1 },
     {
-      field: "Owner_Certificate",
-      headerName: "Owner_Certificate",
+      field: 'Owner_Certificate',
+      headerName: 'Owner_Certificate',
       flex: 1,
       renderCell: (params) => (
         <a href={params.value} target="_blank" rel="noopener noreferrer">
@@ -48,12 +49,15 @@ const Customer_Details = () => {
       <Navbar_Admin />
       <h1 className="text-4xl font-bold text-center mt-10">Customer Details</h1>
       <div className="container mx-auto my-10 flex flex-col gap-4">
-        <TextField
-          label="Search customers"
+        <input
+          type="text"
+          className="form-control"
+          id="searchCustomers"
+          placeholder="Search customers"
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
         />
-        <div style={{ height: 400, width: "100%" }}>
+        <div style={{ height: 400, width: '100%' }}>
           <DataGrid
             rows={filteredcustomers}
             columns={columns}
@@ -72,15 +76,29 @@ const Customer_Details = () => {
       </div>
       {/* Button to add more customer */}
       <div className="flex justify-center my-10">
-        <button
+        {/* <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
             // redirect to add_cusomter
-            navigate("/admin/add_customer");
+            navigate('/admin/add_customer');
           }}
         >
           <AddIcon /> Add Customer
-        </button>
+        </button> */}
+        <Button
+          text={
+            <>
+              <AddIcon />
+              Add Customer
+            </>
+          }
+          onClick={() => {
+            '';
+            // redirect to add_cusomter
+            navigate('/admin/add_customer');
+          }}
+          btn_type="primary"
+        />
       </div>
     </>
   );
